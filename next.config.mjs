cb39fd90cf9726dab18d680120b93ff731a4cc67
neon <neon@gmail.com>
2024-09-22 00:35:10 +0330

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/nextjs', // نام مخزن شما در GitHub
-  assetPrefix: '/nextjs/', // برای تنظیم صحیح مسیر فایل‌های استاتیک
+  // basePath: '/nextjs', // نام مخزن شما در GitHub
+  // assetPrefix: '/nextjs/', // برای تنظیم صحیح مسیر فایل‌های استاتیک
 
   images: {
     domains: ['res.cloudinary.com', 'localhost'], // اضافه کردن 'localhost' برای اجازه دادن به تصاویر لوکال
@@ -11,14 +11,3 @@ const nextConfig = {
 export default nextConfig;
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',  // برای خروجی گرفتن استاتیک
-//   basePath: '/nextjs',  // نام مخزن شما
-//   assetPrefix: '/nextjs/',  // برای درست کردن مسیر فایل‌های استاتیک
-//   images: {
-//     unoptimized: true,  // به دلیل خروجی استاتیک باید از تصاویر بهینه نشده استفاده کنید
-//   },
-// };
-
-// export default nextConfig;
